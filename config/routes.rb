f281d3ad864478eb90eebe4b4337bc8f4cecb8c1
Cloudymolecule <baires82@gmail.com>
2020-12-17 15:36:32 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,41 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'songs/new'
+
+  # get 'songs/create'
+
+  # get 'songs/edit'
+
+  # get 'songs/update'
+
+  # get 'songs/index'
+
+  # get 'songs/show'
+
+  # get 'genres/new'
+
+  # get 'genres/create'
+
+  # get 'genres/edit'
+
+  # get 'genres/update'
+
+  # get 'genres/index'
+
+  # get 'genres/show'
+
+  # get 'artists/new'
+
+  # get 'artists/create'
+
+  # get 'artists/edit'
+
+  # get 'artists/update'
+
+  # get 'artists/index'
+
+  # get 'artists/show'
+
+  resources :artists, only: [:index, :show, :new, :create, :edit, :update]
+  resources :genres, only: [:index, :show, :new, :create, :edit, :update]
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
 end
